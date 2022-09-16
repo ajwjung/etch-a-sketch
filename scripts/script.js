35d581d6ebc3ff2eb0276e416cd8a74f6183bd2a
Amy Jung <ajwjung@outlook.com>
2022-09-16 14:43:25 -0700

    Add hover effect to grid boxes

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,27 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    container.appendChild(row);
-
-    for (let j = 0; j < 16; j++) {
-        const box = document.createElement("div");
-        box.className = "box";
-        row.appendChild(box);
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        container.appendChild(row);
+    
+        for (let j = 0; j < 16; j++) {
+            const box = document.createElement("div");
+            box.className = "box";
+            changeColorOnHover(box);
+            row.appendChild(box);
+        }
     }
 }
 
+function changeColorOnHover(item) {
+    item.addEventListener("mouseover", function (e) {
+        item.style.backgroundColor = "rgb(25, 50, 100)";
+    })
+    item.addEventListener("mouseout", function (e) {
+        item.style.backgroundColor = "transparent";
+    })
+}
+
+createGrid();
